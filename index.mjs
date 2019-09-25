702a626b0e5bc23b1c65c73ea3d6510fc82eaec4
Rich Harris <richard.a.harris@gmail.com>
2019-09-25 18:56:23 -0400

    -> v1.0.1

--- index.mjs
+++ index.mjs
@@ -1,20 +1,72 @@
+let load_image;
+
+if (typeof Blob !== 'undefined' && typeof createImageBitmap !== 'undefined') {
+	const worker_url = URL.createObjectURL(new Blob(
+		[`self.onmessage = e => { self.onmessage = null; eval(e.data); };`],
+		{ type: 'application/javascript' }
+	));
+
+	const worker = new Worker(worker_url);
+
+	const fn = () => {
+		self.onmessage = e => {
+			fetch(e.data, { mode: 'cors' })
+				.then(response => response.blob())
+				.then(data => createImageBitmap(data))
+				.then(bitmap => {
+					self.postMessage({ bitmap }, [bitmap]);
+				})
+				.catch(error => {
+					self.postMessage({
+						error: {
+							message: error.message,
+							stack: error.stack
+						}
+					});
+				});
+		};
+	};
+
+	const code = fn.toString().replace(/^(function.+?|.+?=>\s*)\{/g, '').slice(0, -1);
+	worker.postMessage(code);
+
+	load_image = src => {
+		return new Promise((fulfil, reject) => {
+			worker.onmessage = e => {
+				if (e.data.error) {
+					reject(e.data.error);
+				}
+
+				else fulfil(e.data.bitmap);
+			};
+
+			worker.postMessage(new URL(src, location.href).href);
+		});
+	};
+} else {
+	load_image = src => {
+		return new Promise((fulfil, reject) => {
+			const img = new Image();
+			img.onload = () => fulfil(img);
+			img.onerror = reject;
+			img.src = src;
+		});
+	}
+}
+
+
 export function create(sprite, manifest) {
-	const base = document.createElement('canvas');
-	base.width = sprite.naturalWidth;
-	base.height = sprite.naturalHeight;
-
-	const ctx = base.getContext('2d');
-	ctx.drawImage(sprite, 0, 0);
-
 	const canvas = file => {
 		const b = manifest[file];
 		if (!b) throw new Error(`Could not find ${file} in manifest`);
 
-		const crop = document.createElement('canvas');
-		crop.width = b[2];
-		crop.height = b[3];
+		const [x, y, w, h] = b;
 
-		crop.getContext('2d').putImageData(ctx.getImageData(...b), 0, 0);
+		const crop = document.createElement('canvas');
+		crop.width = w;
+		crop.height = h;
+
+		crop.getContext('2d').drawImage(sprite, x, y, w, h, 0, 0, w, h);
 
 		return crop;
 	};
@@ -33,12 +85,7 @@ export function create(sprite, manifest) {
 export async function load(dir) {
 	const [manifest, sprite] = await Promise.all([
 		fetch(`${dir}/sprite.json`).then(r => r.json()),
-		new Promise((fulfil, reject) => {
-			const img = new Image();
-			img.onload = () => fulfil(img);
-			img.onerror = reject;
-			img.src = `${dir}/sprite.png`;
-		})
+		load_image(`${dir}/sprite.png`)
 	]);
 
 	return create(sprite, manifest);
